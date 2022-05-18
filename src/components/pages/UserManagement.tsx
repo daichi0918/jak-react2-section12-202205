0329f6488925fe7be7d567bce902b361ea10019d
daichi0918 <74330571+daichi0918@users.noreply.github.com>
2022-05-18 20:31:33 +0900

    section78完了

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,18 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+import { UserCard } from "../organisms/user/UserCard";
+
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <UserCard
+          imageUrl="https://static.amanaimages.com/imgroom/rf_preview640/10131/10131006889.jpg"
+          userName="出雲"
+          fullName="Daichi Kayasuga"
+        />
+      </WrapItem>
+    </Wrap>
+  );
 });
