e8ec6ade1c65aa86bddcdf75aea4563497569ffd
daichi0918 <74330571+daichi0918@users.noreply.github.com>
2022-05-19 22:38:32 +0900

    section79完了

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,18 +1,33 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { memo, useEffect, VFC } from "react";
 
 import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        <UserCard
-          imageUrl="https://static.amanaimages.com/imgroom/rf_preview640/10131/10131006889.jpg"
-          userName="出雲"
-          fullName="Daichi Kayasuga"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://static.amanaimages.com/imgroom/rf_preview640/10131/10131006889.jpg"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
   );
 });
